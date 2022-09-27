380c560444e2fa5c17fd1213c6b763018f2739a2
mczionjohnson <87995811+mczionjohnson@users.noreply.github.com>
2022-09-27 21:58:41 +0100

    set up postgres Db

---
+++ queries.js
@@ -0,0 +1,36 @@
+const Pool = require("pg").Pool;
+
+const pool = new Pool({
+    user: 'mczionjohnson',
+    host: 'localhost',
+    database: 'comboard',
+    password: 'abc',
+    port: 5432,
+})
+
+const getPosts = (req, res) => {
+    pool.query('SELECT * FROM artposts', (err, results) => {
+        if (err) {
+            throw err
+        }
+        // res.status(200).json(results.rows)
+        // res.send("Hello Insomnia 2");
+
+        const objects = results.rows;
+        const testid = 1;
+
+        const { team, event } = objects.find(object => object.id === testid);
+
+        console.log(team, event);
+        // return;
+        res.status(200).json(team);
+        return;
+
+    });
+
+    // res.send("Hello Insomnia 2");
+}
+
+module.exports = {
+    getPosts,
+};
--- server.js
+++ server.js
@@ -14,6 +14,12 @@ app.use(cors());
 // mongoose.connect("mongodb://localhost:27017/commBoardReact")
 // .catch((err) => console.log(err));
 
+const db = require('./queries')
 
+// app.get('/', (req, res) => {
+//     res.send("Hello Insomnia")
+// })
+
+app.get('/', db.getPosts)
 
 app.listen(process.env.PORT || 5000, () => console.log("express is running"));
